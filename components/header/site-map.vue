36e0401783f784c68cfb5c1e5c6ef215048e2f10
YALNEZAMI <yaseralnezami@gmail.com>
2024-08-04 00:42:55 +0200

    refact: hide the component when its the root url

--- components/header/site-map.vue
+++ components/header/site-map.vue
@@ -1,6 +1,11 @@
 <template>
-  <main class="flex flex-wrap p-1 border-y-2 border-y-white bg-white w-screen">
-    <div class="mx-1 underline"><NuxtLink to="/">Accueil</NuxtLink></div>
+  <main
+    v-if="getRoutes().length >= 1"
+    class="flex flex-wrap p-1 border-y-2 border-y-white bg-white w-screen"
+  >
+    <div class="mx-1 underline">
+      <NuxtLink to="/">Accueil</NuxtLink>
+    </div>
     <div v-for="route of getRoutes()" :key="route">
       <span>-></span>
       <NuxtLink class="mx-1 underline" :to="'/' + route">{{
@@ -10,7 +15,6 @@
   </main>
 </template>
 <script lang="ts" setup>
-console.log("mina");
 const getRoutes = () => {
   let route = useRoute();
   let path = route.path;
