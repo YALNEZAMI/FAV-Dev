9466b8fa06baab452f228db99a98b91c59fb8ff9
YALNEZAMI <yaseralnezami@gmail.com>
2024-09-07 10:05:01 +0200

    style: adapth padding with the existence or not of the app-map bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,20 @@
 <template>
   <main>
     <Header class="fixed left-0 top-0 w-full z-50"></Header>
-    <div class="bg-pink-100 pt-48 pb-2" style="min-height: 600px">
+    <div
+      class="bg-pink-100 pb-2"
+      :class="{
+        'pt-48': path != '/',
+        'pt-44': path == '/',
+      }"
+      style="min-height: 600px"
+    >
       <slot></slot>
     </div>
     <Footer></Footer>
   </main>
 </template>
+<script lang="ts" setup>
+const route = useRoute();
+const path = route.path;
+</script>
