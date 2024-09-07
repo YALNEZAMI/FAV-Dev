e543b1ea15b6aca76f8c93caf24899adc8d3a01a
YALNEZAMI <yaseralnezami@gmail.com>
2024-09-07 10:08:16 +0200

    restyle: app-map bar more elegant

--- components/header/site-map.vue
+++ components/header/site-map.vue
@@ -3,14 +3,19 @@
     v-if="getRoutes().length >= 1"
     class="flex flex-wrap p-1 border-y-2 border-y-white bg-white w-screen"
   >
-    <div class="mx-1 underline">
-      <NuxtLink to="/">Accueil</NuxtLink>
+    <div class="mx-1">
+      <span>Vous êtes ici : </span>
+      <NuxtLink to="/" class="underline font-bold hover:opacity-80"
+        >Accueil</NuxtLink
+      >
     </div>
     <div v-for="route of getRoutes()" :key="route">
       <span>-></span>
-      <NuxtLink class="mx-1 underline" :to="'/' + route">{{
-        route.charAt(0).toUpperCase() + route.slice(1)
-      }}</NuxtLink>
+      <NuxtLink
+        class="mx-1 underline font-bold hover:opacity-80"
+        :to="'/' + route"
+        >{{ route.charAt(0).toUpperCase() + route.slice(1) }}</NuxtLink
+      >
     </div>
   </main>
 </template>
